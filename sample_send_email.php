0efa7ebb1d3d6384c0305b9197d0fed0cdc04c01
hachiXhachi <hhachiman26@gmail.com>
2023-10-09 14:31:39 +0800

    email prototype and change login.php to index.php

---
+++ sample_send_email.php
@@ -0,0 +1,32 @@
+<?php
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+require "vendor/autoload.php";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "usernameistakenqwerty0@gmail.com";
+$mail->Password = "bghm wjxx hleg gyei";
+
+$mail->addAddress($email);
+$mail->setFrom("usernameistakenqwerty0@gmail.com", "Hachi");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+if ($mail->send()) {
+    echo 'success';
+} else {
+    echo 'error';
+}
+?>
