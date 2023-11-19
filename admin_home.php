49a5cddf83e3add48de326ec473ba838fde8d459
Darknxsss <apigooliver3@gmail.com>
2023-11-19 14:54:45 +0800

    welcome admin

--- admin_home.php
+++ admin_home.php
@@ -3,7 +3,7 @@
   style="font-family:sans-serif;display: flex;align-items: center;justify-content: center;">
   <img src="assets/icon_email.jpg" class="img-fluid" width="70" alt="profile" style="margin-right: 10%;">
   <div class="text-white">
-    <h2>Jian Kyle Albaro
+    <h2>Welcome, ADMIN
       <br>
     </h2>
   </div>
