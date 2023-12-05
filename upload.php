8f3a07ca10eb4d667ada4fdfdbb1100cfe201691
hachiXhachi <75783462+hachiXhachi@users.noreply.github.com>
2023-12-05 15:18:48 +0800

    add year_level on upload

--- upload.php
+++ upload.php
@@ -10,7 +10,7 @@ if ($_FILES["file"]["error"] == 0) {
 
     $headers = fgetcsv($handle, 1000, ",");
     
-    $requiredColumns = ['studentid', 'name', 'sectionid', 'department', 'schoolemail'];
+    $requiredColumns = ['studentid', 'name', 'sectionid', 'department', 'schoolemail', 'year_level'];
 
     $columnPositions = array_flip($headers);
 
@@ -27,6 +27,7 @@ if ($_FILES["file"]["error"] == 0) {
         $sectionid = $data[2];
         $department = $data[3];
         $schoolemail = $data[4];
+        $year_level = $data[5];
 
         // Check if the user with the same studentid already exists
         $result = $con->query("SELECT * FROM student_tbl WHERE studentid='$studentid'");
@@ -34,12 +35,12 @@ if ($_FILES["file"]["error"] == 0) {
 
         if ($existing_user) {
             // Update existing user
-            $stmt = $con->prepare("UPDATE student_tbl SET studentid='$studentid', name='$name', sectionid='$sectionid', department='$department', schoolemail='$schoolemail' WHERE studentid='$studentid'");
+            $stmt = $con->prepare("UPDATE student_tbl SET studentid='$studentid', name='$name', sectionid='$sectionid', department='$department', schoolemail='$schoolemail', year_level='$year_level' WHERE studentid='$studentid'");
             $stmt->execute();
         } else {
             // Insert new user
-            $sql = "INSERT INTO student_tbl(studentid, name, sectionid, department, schoolemail) VALUES (?, ?, ?, ?, ?)";
-            $data = array($studentid, $name, $sectionid, $department, $schoolemail);
+            $sql = "INSERT INTO student_tbl(studentid, name, sectionid, department, schoolemail, year_level) VALUES (?, ?, ?, ?, ?, ?)";
+            $data = array($studentid, $name, $sectionid, $department, $schoolemail, $year_level);
             $stmt = $con->prepare($sql);
             $stmt->execute($data);
         }
